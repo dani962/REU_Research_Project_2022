cc3b168188dbb7f4a88bd762a3cd9b7e4fe64380
Danielle Butler <56207905+dani962@users.noreply.github.com>
2022-06-08 12:17:38 -0400

    Add files via upload

--- REU_Research_Project.R
+++ REU_Research_Project.R
@@ -1,3 +1,16 @@
 library(readr)
 
-data1 <- read_csv("C:/Users/dmbut/Dropbox/PC/Downloads/Greensboro_Fire_Incidents.csv")
+gso.fire <- read_csv("C:/Users/dmbut/Dropbox/PC/Downloads/Greensboro_Fire_Incidents.csv")
+
+class(gso.fire$CallProcessingTime)
+
+library(lubridate)
+
+?lubridate
+
+#convert to Period object
+call_process_period <- lubridate::hms(gso.fire$CallProcessingTime)
+
+class(call_process_period)
+
+call_process_seconds <- period_to_seconds(call_process_period)
